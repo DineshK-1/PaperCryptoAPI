cd0c9b33c20030e282d48ac6e099539bd173a29b
Dinesh <dineshkumar.software@gmail.com>
2023-09-12 17:02:43 +0530

    Added transactions

--- Database/models.py
+++ Database/models.py
@@ -6,12 +6,11 @@ from sqlalchemy.orm import relationship
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     uid = Column(String(100), primary_key=True)
     First_Name = Column(String(100), nullable=False)
     Last_Name = Column(String(100), nullable=False)
     Email = Column(String(200), nullable=False)
-    Phone = Column(Integer)
+    Phone = Column(String(20))
 
     Current_Balance = Column(Integer)
 
@@ -19,6 +18,19 @@ class User(Base):
     
     created_date = Column(DATE, nullable=False, server_default=func.now())
     time_updated = Column(TIMESTAMP(timezone=True), onupdate=func.now())
+
+    transactions_account = relationship('AccountTransactions', backref='user')
+
+class AccountTransactions(Base):
+
+    __tablename__ = "transactions_accounts"
+
+    transaction_id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    user_id = Column(String(100), ForeignKey("users.uid"))
+    transaction_type = Column(String(50), nullable=False)
+    amount = Column(String(30))
+
+    transaction_time = Column(TIMESTAMP(timezone=True), nullable=False, server_default=func.now())
 
 # class Driver(Base):
 #     __tablename__ = "drivers"

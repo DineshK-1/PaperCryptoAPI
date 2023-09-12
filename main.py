ddcbfe61aa692e18d03ab447da2cd4f0ece45ddf
Dinesh <dineshkumar.software@gmail.com>
2023-09-12 17:20:56 +0530

    Added Transactions

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ def get_db():
 async def root():
     return {"message": "Hello World"}
 
-@app.get("/get_user", tags=["User"])
+@app.get("/users/{uid}", tags=["User"])
 def get_user(uid: str,  db: Session = Depends(get_db)):
     user = db.query(models.User).filter(models.User.uid == uid).first()
 
@@ -65,3 +65,51 @@ def create_user(uid: str, First_Name:str, Last_Name: str, Email: str, Phone: str
         raise HTTPException(status_code=500, detail="Error creating user")
 
     return user_object
+
+@app.post("/add_balance", tags=["User"])
+def create_user(uid: str, amount : str,  db: Session = Depends(get_db)):
+    
+    user_obj = db.query(models.User).filter_by(uid=uid).first()
+
+    user_obj.Current_Balance += int(amount)
+
+    transaction = models.AccountTransactions(user_id = uid, transaction_type = "Deposit", amount=amount)
+
+    try:
+        db.add(transaction)
+        db.commit()
+    except SQLAlchemyError as e:
+        print("Error during deposit:", str(e))
+        raise HTTPException(status_code=500, detail="Error during deposit")
+
+    return {"status": "Success"}
+
+@app.post("/withdraw_money", tags=["User"])
+def create_user(uid: str, amount : str,  db: Session = Depends(get_db)):
+    
+    user_obj = db.query(models.User).filter_by(uid=uid).first()
+
+    if(user_obj.Current_Balance >= int(amount)):
+        user_obj.Current_Balance -= int(amount)
+    else:
+        return {"status" : "Failure", "message" : "Not enough funds to withdraw"}
+
+    transaction = models.AccountTransactions(user_id = uid, transaction_type = "Withdrawal", amount=amount)
+
+    try:
+        db.add(transaction)
+        db.commit()
+    except SQLAlchemyError as e:
+        print("Error during withdrawal:", str(e))
+        raise HTTPException(status_code=500, detail="Error during withdrawal")
+
+    return {"status": "Success"}
+
+@app.get("/users/{uid}/fetch_balance", tags=["User"])
+def get_user(uid: str,  db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.uid == uid).first()
+
+    if user:
+        return {"user_id": uid, "balance": user.Current_Balance}
+    else:
+        raise HTTPException(status_code=404, detail=f"User with ID {uid} not found")
